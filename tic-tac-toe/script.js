d6f371bfa15d2b30f87c2494cdce9a86c9655ffe
elahe mottaghin <elahemotaghin@gmail.com>
2021-11-12 20:27:49 +0330

    second commit

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,16 +4,63 @@ let currentTurn = 'X';
 const cells = document.getElementsByClassName('cell');
 const resetButton = document.getElementsByClassName('game--restart')[0];
 let statusTitle = document.getElementsByClassName('game--status')[0];
+let state = initStates();
+let finish = false;
+let title = '';
+
+function initStates (){
+    let state = ['', '', '', '', '', '', '', '', ''];
+    return state;
+}
+
+function isFinish(){
+    for(let i = 0 ; i < 9 ; i += 3){
+        if(state[i] == state[1 + i] && state[1 + i] == state[2 + i] && state[i] != ''){
+            title = 'Player ' + state[i] + ' has won'
+            return true;
+        }
+    }
+    for(let i = 0 ; i < 3 ; i ++){
+        if(state[i] == state[3 + i] && state[i] == state[6 + i] && state[i] != ''){
+            title = 'Player ' + state[i] + ' has won'
+            return true;
+        }
+    }
+
+    if(state[0] == state[4] && state[4] == state[8] && state[0] != ''){
+        title = 'Player ' + state[0] + ' has won'
+        return true;
+    }
+
+    if(state[2] == state[4] && state[4] == state[6] && state[2] != ''){
+        title = 'Player ' + state[2] + ' has won'
+        return true;
+    }
+    let count = 0;
+    for(let i = 0 ; i < 9 ; i++){
+        if(state[i] != '')
+            count++;
+    }
+    if(count == 9){
+        title = 'Game ended in a draw';
+        return true;
+    }
+    return false;
+}
 
 const changeTurn = () =>{
     if(currentTurn === 'X')
         currentTurn = 'O';
     else
         currentTurn = 'X';
+    finish = isFinish();
 }
 
 const fillStatusTilte = () =>{
-    if(currentTurn === 'X')
+    if(finish){
+        statusTitle.innerHTML = title;
+    }
+    else if(currentTurn === 'X')
         statusTitle.innerHTML = "It's X's turn";
     else
         statusTitle.innerHTML = "It's O's turn";
@@ -21,15 +68,23 @@ const fillStatusTilte = () =>{
 
 const fillCell = (event) => {
     let currentCell =  event.target;
-    currentCell.innerHTML = currentTurn;
-    changeTurn();
-    fillStatusTilte();
+    let index =currentCell.getAttribute('data-cell-index');
+    if(state[index] == '' && !finish){
+        state[index] = currentTurn;
+        currentCell.innerHTML = currentTurn;
+        changeTurn();
+        fillStatusTilte();
+    }
 }
 
 const reset = () => {
     for (const cell of cells) {
         cell.innerHTML = '';
     }
+    currentTurn = 'X';
+    finish = false;
+    fillStatusTilte();
+    state = initStates();
 }
 
 const eventHandler = () => {
